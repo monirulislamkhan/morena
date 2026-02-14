79450d51446031c9c785531f124eff57d84fd0ad
monirulislamkhan <mr.monirulkhan@gmail.com>
2026-02-14 13:41:54 +0530

    working in home page why choose section

--- index.php
+++ index.php
@@ -134,11 +134,61 @@ include("include/var.inc.php");
     </section>
 
 
+    <section class="projects">
+      <div class="container-xl">
+        <h2 class="mb-4 fw-bold text-center section-title">Recommended Projects</h2>
+        <div class="row">
+          <div class="col-lg-6">
+            <figure class="projects__items">
+              <img src="https://topultraluxury.com/wp-content/uploads/damac-valencia-main-600x400.avif" class="img-fluid" alt="DAMAC Valencia">
+              <figcaption>
+                <div>
+                  <h2 class="mb-0">
+                    <a href="https://topultraluxury.com/new-projects/damac-valencia/" class="stretched-link text-dark" style="text-decoration: none;">
+                      DAMAC Valencia </a>
+                  </h2>
+                  <div class="location" style="font-size: 1rem;">
+                    <i class="fas fa-map-marker-alt fa-lg"></i>
+                    DAMAC Lagoons, Dubai
+                    <div class="price-value mt-2">
+                      AED 725,000
+                    </div>
+                  </div>
+
+                </div>
+
+              </figcaption>
+            </figure>
+          </div>
+          <div class="col-lg-6">
+            <figure class="projects__items">
+              <img src="https://topultraluxury.com/wp-content/uploads/sobha-sanctuary-main-600x400.avif" class="img-fluid" alt="Sobha Sanctuary">
+              <figcaption>
+                <div>
+                  <h2 class="mb-0">
+                    <a href="https://topultraluxury.com/new-projects/sobha-sanctuary/" class="stretched-link text-dark" style="text-decoration: none;">
+                      Sobha Sanctuary </a>
+                  </h2>
+                  <div class="location" style="font-size: 1rem;">
+                    <i class="fas fa-map-marker-alt fa-lg"></i>
+                    DubaiLand, Dubai
+                    <div class="price-value mt-2">
+                      AED 3,800,000
+                    </div>
+                  </div>
+
+                </div>
+
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <section class="properties">
       <div class="container-xl">
-        <div class="h2  mb-0">Our Latest Projects</div>
-        <p class="sub">Are you looking for right Property?</p>
+        <div class="h2">Latest Off-Plan Projects </div>
         <div class="row">
           <div class="col-lg-4">
             <figure class="properties__items">
@@ -203,6 +253,251 @@ include("include/var.inc.php");
                 </div>
               </figcaption>
             </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="communities">
+      <div class="container-xl">
+        <h2 class="mb-4 text-center section-title">Explore Top Communities</h2>
+        <div class="row">
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/abu-dhabi/al-bahiya/">
+                <img src="https://via.placeholder.com/500x300?text=Community+Image" class="card-img-top" alt="Al Bahiya">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Bahiya</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/dubai/al-furjan/">
+                <img src="https://topultraluxury.com/wp-content/uploads/Al-Furjan-2.avif" class="card-img-top" alt="Al Furjan">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Furjan</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/ras-al-khaimah/al-hamra/">
+                <img src="https://topultraluxury.com/wp-content/uploads/Al-Hamra-image.avif" class="card-img-top" alt="Al Hamra">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Hamra</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/ras-al-khaimah/al-hamra-village/">
+                <img src="https://topultraluxury.com/wp-content/uploads/Al-Hamra-Village.avif" class="card-img-top" alt="Al Hamra Village">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Hamra Village</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/dubai/al-jaddaf/">
+                <img src="https://topultraluxury.com/wp-content/uploads/al-jaddaf-walk-image.avif" class="card-img-top" alt="Al Jaddaf">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Jaddaf</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 mb-4">
+            <div class="card community-card h-60">
+              <a href="https://topultraluxury.com/communities/abu-dhabi/al-jurf/">
+                <img src="https://via.placeholder.com/500x300?text=Community+Image" class="card-img-top" alt="Al Jurf">
+              </a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Al Jurf</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center">
+          <a href="/communities/" class="btn btn-outline-primary">All Communities in UAE <i>→</i></a>
+        </div>
+      </div>
+    </section>
+
+    <section class="category-section">
+      <div class="container-xl">
+        <h2 class="mb-4 fw-bold text-center section-title">Explore by Property Type</h2>
+        <div class="row g-4">
+
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/apartments/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://topultraluxury.com/wp-content/uploads/building-4249846_640.avif'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  231 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Apartments</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/commercial/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://via.placeholder.com/600x400?text=Commercial'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  4 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Commercial</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/duplex/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://topultraluxury.com/wp-content/uploads/duplex.avif'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  32 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Duplex</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/mansion/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://topultraluxury.com/wp-content/uploads/mension-image.avif'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  7 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Mansion</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/penthouse/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://topultraluxury.com/wp-content/uploads/Penthouse-1.avif'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  33 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Penthouse</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+          <div class="col-md-4 col-lg-4">
+            <a href="https://topultraluxury.com/property/royal-suites/" class="text-decoration-none text-white">
+              <div class="card text-white border-0 shadow-sm h-100 rounded overflow-hidden position-relative"
+                style="background-image: url('https://via.placeholder.com/600x400?text=Royal%20Suites'); background-size: cover; background-position: center; min-height: 300px;">
+
+
+                <span class="position-absolute bottom-0 start-0 m-3 badge bg-warning text-dark px-3 py-2 small shadow" style="z-index: 2;">
+                  1 Projects
+                </span>
+
+                <div class="card-body d-flex flex-column justify-content-end"
+                  style="padding-bottom: 70px; background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 70%);">
+                  <h4 class="fw-bold mb-0">Royal Suites</h4>
+                </div>
+              </div>
+
+            </a>
+          </div>
+
+        </div>
+      </div>
+      <div class="text-center mt-4">
+        <a href="/property/" class="btn btn-outline-primary">View All Property <i>→</i></a>
+      </div>
+    </section>
+
+    <section class="why-choose-us py-5 bg-light-subtle">
+      <div class="container-xl">
+        <div class="text-center mb-5">
+          <h2 class="fw-bold section-title">Why Choose Us?</h2>
+          <p class="text-muted">Your trusted partner in UAE’s most exclusive investments.</p>
+        </div>
+
+        <div class="row g-4 bg-light-subtle">
+
+          <div class="col-md-4">
+            <div class="card h-70 text-center p-4 border-0 shadow-sm bg-dark text-white rounded-3">
+              <div class="mb-3">
+                <i class="bi bi-emoji-smile text-warning big-icon"></i>
+              </div>
+              <h5 class="fw-bold">Trusted By Thousands</h5>
+              <p class="mb-0">
+                Built on years of trust, serving buyers, sellers, and investors in Dubai.
+              </p>
+            </div>
+          </div>
+
+
+          <div class="col-md-4">
+            <div class="card h-70 text-center p-4 border-0 shadow-sm bg-dark text-white rounded-3">
+              <div class="mb-3">
+                <i class="bi bi-calculator text-warning big-icon"></i>
+              </div>
+              <h5 class="fw-bold">Financing Made Easy</h5>
+              <p class="mb-0">
+                We aim to simplify the financing process by partnering with leading banks and
+                financial institutions.
+              </p>
+            </div>
+          </div>
+
+
+          <div class="col-md-4">
+            <div class="card h-70 text-center p-4 border-0 shadow-sm bg-dark text-white rounded-3">
+              <div class="mb-3">
+                <i class="bi bi-house text-warning big-icon"></i>
+              </div>
+              <h5 class="fw-bold">Wide Range Of Properties</h5>
+              <p class="mb-0">
+                Explore an extensive portfolio of prime properties in UAE’s
+                most sought-after locations.
+              </p>
+            </div>
           </div>
         </div>
       </div>
